bdee711d4aa48b4b50045ffc5beaedb4a39d421e
Paweł <pawcio16m@gmail.com>
2015-06-03 00:00:26 +0200

    Dobra przedtem sie nie udalo ale teraz powinno

---
+++ src/MainActivity.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Pawel on 2015-06-02.
+ */
+//This is a main activity in our project
+public class MainActivity {
+    public static void main(String[] args) {
+        System.out.println("Witaj w systemie!");
+        System.out.println("Witaj w systemie!");
+        System.out.println("Witaj w systemie!");
+
+
+    }
+}
